0ab8ccc6dc0b9dce92b53cf0556b00358f3eb374
mirneishon <mirnavrl@gmail.com>
2019-11-12 10:23:36 -0600

    modificando la funcion para leer el objeto

--- R/clustering_kos.R
+++ R/clustering_kos.R
@@ -1,7 +1,16 @@
-clustering_kos<-function(objPhyloseq){
+clustering_kos<-function(objPhyloseq, estadio){
   wh0otu = genefilter_sample(objPhyloseq, filterfun_sample(function(x) x > 5), A=0.5*nsamples(objPhyloseq))
   GP1otu = prune_taxa(wh0otu, objPhyloseq)
   GP1otu = transform_sample_counts(GP1otu, function(x) 1E6 * x/sum(x))
+  if (is.character(estadio) == TRUE ){
   pseq.1otu <- subset_samples(GP1otu, SampleType == "Larvae")
+  }
+  pdf("OTUclustering.pdf", width=10, height=10)
+  orduotu = ordinate(pseq.1otu, "PCoA", "bray")
+  potu = plot_ordination(pseq.1otu, orduotu, color="Host")
+  potu = potu + geom_point(size=3, alpha=0.75)
+  potu = potu + scale_colour_brewer(type="qual", palette="Set1")
+  potu + ggtitle("MDS/PCoA on Bray distance, Otus")
+  dev.off()
 
 }
--- R/read_phyloseq.R
+++ R/read_phyloseq.R
@@ -1,3 +1,6 @@
-read_phyloseq<- function(objPhyloseq){
-  message("Reading a phyloseq object")
+read_to_phyloseq<- function(mapping, biom){
+  map=import_qiime_sample_data("mapping")
+  otus=import_biom("biom", parseFunction=parse_taxonomy_greengenes)
+  mergedata=merge_phyloseq(otus,map)
+  message("Creating a phyloseq object")
 }
