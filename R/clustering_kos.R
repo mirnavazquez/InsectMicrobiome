652c6d23bbe2ac20d9df77d99f8a18a6dc6cef99
mirneishon <mirnavrl@gmail.com>
2019-11-12 09:55:00 -0600

    haciendo la funcion

--- R/clustering_kos.R
+++ R/clustering_kos.R
@@ -1,3 +1,7 @@
 clustering_kos<-function(objPhyloseq){
+  wh0otu = genefilter_sample(objPhyloseq, filterfun_sample(function(x) x > 5), A=0.5*nsamples(objPhyloseq))
+  GP1otu = prune_taxa(wh0otu, objPhyloseq)
+  GP1otu = transform_sample_counts(GP1otu, function(x) 1E6 * x/sum(x))
+  pseq.1otu <- subset_samples(GP1otu, SampleType == "Larvae")
 
 }
