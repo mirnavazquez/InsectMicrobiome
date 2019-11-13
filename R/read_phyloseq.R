be1dabe9429e65776fa09da3e9623bf3eb6a74d1
mirneishon <mirnavrl@gmail.com>
2019-11-13 04:46:51 -0600

    llamada a phyloseq

--- R/read_phyloseq.R
+++ R/read_phyloseq.R
@@ -1,6 +1,6 @@
 read_to_phyloseq<- function(mapping, biom){
-  map=import_qiime_sample_data("mapping")
-  otus=import_biom("biom", parseFunction=parse_taxonomy_greengenes)
-  mergedata=merge_phyloseq(otus,map)
+  map=phyloseq::import_qiime_sample_data("mapping")
+  otus=phyloseq::import_biom("biom", parseFunction=parse_taxonomy_greengenes)
+  mergedata=phyloseq::merge_phyloseq(otus,map)
   message("Creating a phyloseq object")
 }
