4213e95ca3a9b3b572050a085ef8e34f078b3e95
Gabriel Yaxal Ponce Soto <yaxal@iecologia.unam.mx>
2019-11-13 20:44:41 +0100

    Set 'mapping' as variable instead of string

--- R/read_phyloseq.R
+++ R/read_phyloseq.R
@@ -1,5 +1,5 @@
 read_to_phyloseq<- function(mapping, biom){
-  map=phyloseq::import_qiime_sample_data("mapping")
+  map=phyloseq::import_qiime_sample_data(mapping)
   otus=phyloseq::import_biom("biom", parseFunction=parse_taxonomy_greengenes)
   mergedata=phyloseq::merge_phyloseq(otus,map)
   message("Creating a phyloseq object")
