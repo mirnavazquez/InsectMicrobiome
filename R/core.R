8789eb5d6ae27b93b53051d8e1358bf79e005499
mirneishon <mirnavrl@gmail.com>
2019-11-14 10:14:17 -0600

    Include an argument that allows you to make a subset.

--- R/core.R
+++ R/core.R
@@ -1,7 +1,40 @@
-core<-function(objtPhyloseq){
-  pseq.2 <- phyloseq::prune_taxa(phyloseq::taxa_sums(objtPhyloseq) > 0, objtPhyloseq)
-  pseq.1 <- phyloseq::subset_samples(pseq.2, SampleType == "Larvae" )
+core<-function(objtPhyloseq, x){
+  #pseq.2 <- phyloseq::prune_taxa(phyloseq::taxa_sums(objtPhyloseq) > 0, objtPhyloseq)
+  if(as.character(x) == TRUE ) {
+  pseq.1 <- phyloseq::subset_samples(objtPhyloseq, SampleType == x )
   pseq.rel <- microbiome::transform(pseq.1, "compositional")
+  pseq.rel <- microbiome::transform(objtPhyloseq, "compositional")
+  core.taxa.standard <- microbiome::core_members(pseq.rel, detection = 0, prevalence = .5)
+  pseq.core <- microbiome::core(pseq.rel, detection = 0, prevalence = .5)
+  core.taxa <- microbiome::taxa(pseq.core)
+  tax.mat <- phyloseq::tax_table(pseq.core)
+  tax.df=as.data.frame(pseq.core@tax_table@.Data)
+  tax.df$OTU <- rownames(tax.df)
+  core.taxa.class <- dplyr::filter(tax.df, rownames(tax.df) %in% core.taxa)
+  knitr::kable(head(core.taxa.class))
+  detections <- 10^seq(log10(1e-5), log10(.2), length = 10)
+  prevalences <- seq(.05, 1, .05)
+  healthycore <-  microbiome::plot_core(pseq.rel, plot.type = "heatmap",
+                                        prevalences = prevalences,
+                                        detections = detections,
+                                        colours = rev(RColorBrewer::brewer.pal(5, "Spectral")),
+                                        min.prevalence = .5, horizontal = F)
+  df <- healthycore$data
+  list <- df$Taxa
+  tax <- phyloseq::tax_table(pseq.1)
+  tax=as.data.frame(pseq.1@tax_table@.Data)
+  tax$OTU <- rownames(tax)
+  tax2 <- dplyr::filter(tax, rownames(tax) %in% list)
+  tax.unit <- tidyr::unite(tax2, Taxa_level,c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "OTU"), sep = "_;", remove = TRUE)
+  tax.unit$Taxa_level <- gsub(pattern="[a-z]__",replacement="", tax.unit$Taxa_level)
+  df$Taxa <- tax.unit$Taxa_level
+  knitr::kable(head(df))
+  df2<-df[order(-df$Prevalence, -df$DetectionThreshold), ]
+  healthycore$data <- df2
+  pdf("core.pdf", width=10, height=10)
+  plot(healthycore + ggplot2::theme(axis.text.y = ggplot2::element_text(face="italic")) + viridis::scale_fill_viridis())
+  } else {
+  pseq.rel <- microbiome::transform(objtPhyloseq, "compositional")
   core.taxa.standard <- microbiome::core_members(pseq.rel, detection = 0, prevalence = .5)
   pseq.core <- microbiome::core(pseq.rel, detection = 0, prevalence = .5)
   core.taxa <- microbiome::taxa(pseq.core)
@@ -19,8 +52,8 @@ core<-function(objtPhyloseq){
                            min.prevalence = .5, horizontal = F)
   df <- healthycore$data
   list <- df$Taxa
-  tax <- phyloseq::tax_table(pseq.2)
-  tax=as.data.frame(pseq.2@tax_table@.Data)
+  tax <- phyloseq::tax_table(objtPhyloseq)
+  tax=as.data.frame(objtPhyloseq@tax_table@.Data)
   tax$OTU <- rownames(tax)
   tax2 <- dplyr::filter(tax, rownames(tax) %in% list)
   tax.unit <- tidyr::unite(tax2, Taxa_level,c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "OTU"), sep = "_;", remove = TRUE)
@@ -32,4 +65,5 @@ core<-function(objtPhyloseq){
   pdf("core.pdf", width=10, height=10)
   plot(healthycore + ggplot2::theme(axis.text.y = ggplot2::element_text(face="italic")) + viridis::scale_fill_viridis())
   dev.off()
+  }
 }
