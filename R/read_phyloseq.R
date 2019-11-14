97ae7eebdd5ec820fade104a1c0ec9d2483f6097
Gabriel Yaxal Ponce Soto <yaxal@iecologia.unam.mx>
2019-11-14 21:04:24 +0100

    Sinchronization

--- R/read_phyloseq.R
+++ R/read_phyloseq.R
@@ -1,6 +1,8 @@
 read_to_phyloseq<- function(mapping, biom){
+  message(mapping)
+  message(biom)
   map=phyloseq::import_qiime_sample_data(mapping)
-  otus=phyloseq::import_biom("biom", parseFunction=parse_taxonomy_greengenes)
+  otus=phyloseq::import_biom(biom, parseFunction=parse_taxonomy_greengenes)
   mergedata=phyloseq::merge_phyloseq(otus,map)
   message("Creating a phyloseq object")
 }
